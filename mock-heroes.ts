d54c13df39235ccf87ab0f5374a444e2a41458db
WojcikKrystian <krystianwojcik@interia.eu>
2018-02-04 23:36:21 +0100

    First commit

---
+++ mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'Andriu' },
+    {id: 2, name: 'Boggy' },
+    {id: 3, name: 'Clue' },
+    {id: 4, name: 'Davis' },
+    {id: 5, name: 'Emanuel' },
+    {id: 6, name: 'Flip' },
+    {id: 7, name: 'Grammy' },
+    {id: 8, name: 'Helix' },
+    {id: 9, name: 'Ivan' },
+    {id: 10, name: 'Juan' }
+];
